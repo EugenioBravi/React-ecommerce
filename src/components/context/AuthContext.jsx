0b6db4d89098f14261e5fab97e116c73f5382c3b
EugenioBravi <bravieugenio@gmail.com>
2022-10-28 17:45:34 -0300

    add reset password

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,13 +1,45 @@
-import { createContext, useContext } from "react";
-
+import {
+  signOut,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  sendPasswordResetEmail
+} from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../../Firebase/Index";
 const AuthContext = createContext();
-export const useAuthContext = useContext(AuthContext);
+export const useAuthContext = () => useContext(AuthContext);
 export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-    const user = {
+  const signup = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    }
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
+  const logOut = () => signOut(auth);
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const loginWithGoogle = () => {
+    const googleProvider = new GoogleAuthProvider();
+    return signInWithPopup(auth, googleProvider);
+  };
+  const resetPassword = async (email) => sendPasswordResetEmail(auth, email);
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ signup, login, logOut, user, loginWithGoogle, resetPassword}}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
